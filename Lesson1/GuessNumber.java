9d864d71730ff6aba103567cd0b0c0ea552c2eeb
Lev Klimov <klimoff.lev@yandex.ru>
2018-06-22 19:47:58 +0300

    Тестовая версия

---
+++ Lesson1/GuessNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner; // идёт импорт сканера
+
+public class GuessNumber {
+	public static void main(String[] args){
+		int number;
+		int a = (int) ( Math.random() * 100 );
+		System.out.println("Сыграем в игру 'Угадай число'");
+		System.out.println("Введите число от 0 до 100");
+		Scanner scan = new Scanner(System.in);
+		do {
+			 number = scan.nextInt();
+			if (number == a){
+			System.out.println("Вы угадали!");
+			}
+			else if (number > a){
+			System.out.println("Введенное вами число больше того, что загадал компьютер");
+			}
+			else {
+			System.out.println("Введенное вами число меньше того, что загадал компьютер");
+		}
+		} while (number == a);
+	}
+}
